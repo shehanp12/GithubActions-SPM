ec5d645ba2407995caa2daa960b6a4317922b218
Roshan Piumal <shehanp068@gmail.com>
2021-10-10 12:18:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="foo">bar</div>
-      </header>
+      
+       Hello World
+      
     </div>
   );
 }
