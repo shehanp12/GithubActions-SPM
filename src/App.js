de871aaf06f9dd69667e8fc697d0dade8dcfab4d
Roshan Piumal <shehanp068@gmail.com>
2021-10-10 12:08:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> TESTING GITHUB ACTIONS WITH REACT </h1>        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    React actions
     </div>
   );
 }
